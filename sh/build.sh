5aa91c23b1cc6c34907a584486601ebb0e05c70f
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-11-17 14:57:08 +0000

    Modified build scripts.

--- sh/build.sh
+++ sh/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # Build GUIdi
-# Usage: "sh/build.sh"
+# Usage: [sudo] "sh/build.sh"
 #export FAN_ENV=util::PathEnv
 #export FAN_ENV_PATH=.
+
+# Except on Cygwin, this most likely will need to be run as root
+if [[ `uname` != "CYGWIN*" ]]; then
+	if [[ "$(id -u)" != "0" ]]; then
+		echo "You must run this as root."
+		echo "e.g. \"sudo sh/build.sh\""
+		exit 1
+	fi
+fi
 fan src/build_pod.fan
+
