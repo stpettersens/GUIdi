e8e93bea6c260a56ccdc29670b394fdacf24eaf4
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-11-17 19:41:58 +0000

    Updated sh/build.sh for Cygwin without root.

--- sh/build.sh
+++ sh/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Build GUIdi
 # Usage: [sudo] "sh/build.sh"
-#export FAN_ENV=util::PathEnv
-#export FAN_ENV_PATH=.
+
+build() {
+	fan src/build_pod.fan
+}
 
 # Except on Cygwin, this most likely will need to be run as root
-if [[ `uname` != "CYGWIN*" ]]; then
+if [[ `uname` =~ CYGWIN.+ ]]; then
+	build
+else
 	if [[ "$(id -u)" != "0" ]]; then
 		echo "You must run this as root."
 		echo "e.g. \"sudo sh/build.sh\""
-		exit 1
 	fi
 fi
-fan src/build_pod.fan
-
